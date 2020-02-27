507d88a5c4e45a69e9b0328cc3212d14b11dcec1
Joseph Cecil <cecil@ads.isi.edu>
2020-02-26 19:33:35 -0500

    Write the game board implementation.

---
+++ public/scripts/index.js
@@ -0,0 +1,139 @@
+const canvas = document.getElementsByClassName("viewport")[0];
+const ctx = canvas.getContext('2d');
+
+// The size of each tile on the screen, in Canvas coordinates.
+const tileSize = 24;
+const DEFAULT_BOARD_SIZE = 18;
+const DEAD_COLOR = 'white';
+const ALIVE_COLOR = 'green';
+const MAX_ADJACENT_TILES = 8;
+
+// Birth and survival rules for Conway's Game of Life.
+const CONWAY_BIRTH = [false, false, false, true, false, false, false, false, false];
+const CONWAY_SURVIVAL = [false, false, true, true, false, false, false, false, false];
+
+GameBoardProto = {
+  board: Array(DEFAULT_BOARD_SIZE * DEFAULT_BOARD_SIZE).fill(false),
+  swapBoard: Array(DEFAULT_BOARD_SIZE * DEFAULT_BOARD_SIZE).fill(false),
+  boardSize: {
+    width: DEFAULT_BOARD_SIZE,
+    height: DEFAULT_BOARD_SIZE
+  },
+  birthRule: CONWAY_BIRTH,
+  survivalRule: CONWAY_SURVIVAL,
+
+  // Initialize the board and canvas.
+  initialize() {
+    canvas.width = this.boardSize.width * tileSize;
+    canvas.height = this.boardSize.height * tileSize;
+  },
+
+  // Clear the board.
+  clear() {
+    this.board.fill(false);
+  },
+
+  // Get the row and column of board item i.
+  getRowCol(i) {
+    const row = Math.floor(i / this.boardSize.width);
+    const col = i % this.boardSize.width;
+    return [row, col]
+  },
+
+  // Get the index of the board item at the given row and column.
+  boardIndex(row, col) {
+    return row * this.width + col;
+  },
+
+  // Resize the board to the given width and height.
+  resize(width, height) {
+    // Create a new empty board and copy over all the tiles from the existing
+    // board.
+    let newBoard = Array(width * height).fill(false);
+    for (let i = 0; i < this.board.length; i++) {
+      const [row, col] = this.getRowCol(i);
+      if ((row * width + col) < newBoard.length) {
+        newBoard[row * width + col] = this.board[i];
+      }
+    }
+
+    this.board = newBoard;
+    this.boardSize = {width, height};
+    this.swapBoard = Array(width * height).fill(false);
+  },
+
+  // Compute the next state for Conway's game of life, writing the state to
+  // swapBoard.
+  calculateStep(swapBoard) {
+    for (let i = 0; i < this.board.length; i++) {
+      let neighbors = neighborsAlive();
+      let lives;
+
+      // If alive, use survival rule
+      if (board[i]) {
+        swapBoard[i] = this.survivalRule[neighbors];
+      }
+      
+      // If dead, use birth rule.
+      else {
+        swapBoard[i] = this.survivalRule[neighbors];
+      }
+    }
+  },
+
+  step() {
+    calculateStep(this.swapBoard);
+    
+    // Swap the boards.
+    let newSwap = this.board;
+    this.board = this.swapBoard;
+    this.swapBoard = newSwap;
+  },
+
+  render() {
+    // First clear the context.
+    ctx.fillStyle = DEAD_COLOR;
+    ctx.fillRect(0, 0, ctx.width, ctx.height);
+
+    // Now render the board.
+    for (let i = 0; i < this.board.length; i++) {
+      if (this.board[i]) {
+        const [row, col] = this.getRowCol(i);
+        ctx.fillStyle = ALIVE_COLOR;
+        ctx.fillRect(col*tileSize, row*tileSize, tileSize, tileSize);
+      }
+    }
+  }
+}
+
+function GameBoard(width, height, birthRule, survivalRule) {
+  if (birthRule.length !== MAX_ADJACENT_TILES + 1) {
+    console.error("Bad birth rule!");
+    return {};
+  }
+
+  if (survivalRule.length !== MAX_ADJACENT_TILES + 1) {
+    console.error("Bad survival rule!");
+    return {};
+  }
+
+  // Set up the board.
+  let board = Object.assign({}, GameBoardProto);
+  board.resize(width, height);
+  board.birthRule = birthRule.slice();
+  board.survivalRule = survivalRule.slice();
+  return board;
+}
+
+function defaultGameBoard() {
+  let board = GameBoard(
+    DEFAULT_BOARD_SIZE, DEFAULT_BOARD_SIZE, CONWAY_BIRTH, CONWAY_SURVIVAL
+  );
+  board.board[8] = true;  // DEBUGGING ONLY
+  return board;
+}
+
+let gameBoard = defaultGameBoard();
+gameBoard.initialize();
+console.error(gameBoard.board);
+gameBoard.render();
