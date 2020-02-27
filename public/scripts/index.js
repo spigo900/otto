c39292179db9c1d7668adbc897db3882b1f1a7ba
Joseph Cecil <cecil@ads.isi.edu>
2020-02-26 20:10:09 -0500

    Implement step button.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementsByClassName("viewport")[0];
+const canvas = document.getElementsByClassName('viewport')[0];
 const ctx = canvas.getContext('2d');
 
 // The size of each tile on the screen, in Canvas coordinates.
@@ -34,7 +34,7 @@ GameBoardProto = {
   },
 
   // Get the row and column of board item i.
-  getRowCol(i) {
+  rowColOf(i) {
     const row = Math.floor(i / this.boardSize.width);
     const col = i % this.boardSize.width;
     return [row, col]
@@ -42,7 +42,7 @@ GameBoardProto = {
 
   // Get the index of the board item at the given row and column.
   boardIndex(row, col) {
-    return row * this.width + col;
+    return row * this.boardSize.width + col;
   },
 
   // Resize the board to the given width and height.
@@ -51,7 +51,7 @@ GameBoardProto = {
     // board.
     let newBoard = Array(width * height).fill(false);
     for (let i = 0; i < this.board.length; i++) {
-      const [row, col] = this.getRowCol(i);
+      const [row, col] = this.rowColOf(i);
       if ((row * width + col) < newBoard.length) {
         newBoard[row * width + col] = this.board[i];
       }
@@ -62,15 +62,42 @@ GameBoardProto = {
     this.swapBoard = Array(width * height).fill(false);
   },
 
+  neighborsAlive(i) {
+    // First, figure out which tiles are neighbors of tile i.
+    const lastRow = this.boardSize.height - 1;
+    const lastCol = this.boardSize.width - 1;
+
+    const [row, col] = this.rowColOf(i);
+    const up = (row > 0) ? (row - 1) : lastRow;
+    const down = (row < lastRow) ? (row + 1) : 0;
+    const left = (col > 0) ? (col - 1) : lastCol;
+    const right = (col < lastCol) ? (col + 1) : 0;
+
+    const neighbors = [
+      [  up, left], [  up, col], [  up, right],
+      [ row, left],              [ row, right],
+      [down, left], [down, col], [down, right]
+    ];
+
+    // Now compute how many of the neighboring tiles are alive.
+    let alive = 0;
+    for (let i = 0; i < neighbors.length; i++) {
+      let [neighborRow, neighborCol] = neighbors[i];
+      if (this.board[this.boardIndex(neighborRow, neighborCol)]) {
+        alive += 1;
+      }
+    }
+    return alive;
+  },
+
   // Compute the next state for Conway's game of life, writing the state to
   // swapBoard.
   calculateStep(swapBoard) {
     for (let i = 0; i < this.board.length; i++) {
-      let neighbors = neighborsAlive();
-      let lives;
+      let neighbors = this.neighborsAlive(i);
 
       // If alive, use survival rule
-      if (board[i]) {
+      if (this.board[i]) {
         swapBoard[i] = this.survivalRule[neighbors];
       }
       
@@ -82,7 +109,7 @@ GameBoardProto = {
   },
 
   step() {
-    calculateStep(this.swapBoard);
+    this.calculateStep(this.swapBoard);
     
     // Swap the boards.
     let newSwap = this.board;
@@ -93,12 +120,12 @@ GameBoardProto = {
   render() {
     // First clear the context.
     ctx.fillStyle = DEAD_COLOR;
-    ctx.fillRect(0, 0, ctx.width, ctx.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // Now render the board.
     for (let i = 0; i < this.board.length; i++) {
       if (this.board[i]) {
-        const [row, col] = this.getRowCol(i);
+        const [row, col] = this.rowColOf(i);
         ctx.fillStyle = ALIVE_COLOR;
         ctx.fillRect(col*tileSize, row*tileSize, tileSize, tileSize);
       }
@@ -108,12 +135,12 @@ GameBoardProto = {
 
 function GameBoard(width, height, birthRule, survivalRule) {
   if (birthRule.length !== MAX_ADJACENT_TILES + 1) {
-    console.error("Bad birth rule!");
+    console.error('Bad birth rule!');
     return {};
   }
 
   if (survivalRule.length !== MAX_ADJACENT_TILES + 1) {
-    console.error("Bad survival rule!");
+    console.error('Bad survival rule!');
     return {};
   }
 
@@ -129,11 +156,38 @@ function defaultGameBoard() {
   let board = GameBoard(
     DEFAULT_BOARD_SIZE, DEFAULT_BOARD_SIZE, CONWAY_BIRTH, CONWAY_SURVIVAL
   );
-  board.board[8] = true;  // DEBUGGING ONLY
   return board;
 }
 
-let gameBoard = defaultGameBoard();
+function randomizedGameBoard() {
+  const p = 0.5;
+  let gameBoard = defaultGameBoard();
+  let board = gameBoard.board;
+
+  for (let i = 0; i < board.length; i++) {
+    board[i] = (Math.random() < p);
+  }
+
+  return gameBoard;
+}
+
+let gameBoard = randomizedGameBoard();
 gameBoard.initialize();
-console.error(gameBoard.board);
 gameBoard.render();
+
+
+//////////////
+// CONTROLS //
+//////////////
+
+// TODO: Click canvas to alter the state of a game tile.
+
+// Step button
+let stepButton = document.getElementsByClassName('automaton-step')[0];
+stepButton.addEventListener('click', () => {
+  gameBoard.step();
+  gameBoard.render();
+  
+}, false)
+
+// TODO: 
